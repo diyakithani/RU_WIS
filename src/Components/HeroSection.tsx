25a4ff82c80ba78d716206df9ef7c6b9520e6455
Diya Kithani <diyakithani.2003@gmail.com>
2024-07-20 10:18:35 -0400

    Vid added to herosection

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import vid1 from "../assets/video1.png"
 
 const HeroSection = () => {
   return (
@@ -10,11 +9,16 @@ const HeroSection = () => {
           All Electrical Services. </span>
       </h1>
       <p className="mt-10 text-xl text-center "> Quality Work, Outstanding Service and a Solution to all your Electrical Problems at an Affordable Price!</p>
-
-
-
+      <div className="flex justify-center my-10">
+        <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">Book Us Now!</a>
+      </div>
+      <div className="flex mt-5 justify-center">
+        <video autoPlay loop muted className="rounded-lg w-1/3 border border-orange-700 shadow-orange-10 mx-2 my-2">
+          <source src="/videos/video1.mp4" type="video/mp4"></source></video>
+      </div>
 
     </div >
+
   );
 };
 
